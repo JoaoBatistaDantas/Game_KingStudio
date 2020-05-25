12cd9b424166940528f3d7e07aee37cbe5dea0dd
AllenRodger <44985411+AllenRodger@users.noreply.github.com>
2020-05-24 22:37:54 -0300

    alteracoes na mobilidade
    coloquei double jump, just time in jump e long jump.

--- Game_version2/scripts/Player.gd
+++ Game_version2/scripts/Player.gd
@@ -1,68 +1,77 @@
 extends KinematicBody2D
 
+# Declaração de variáveis
 const up = Vector2(0,-1)
-export var gravidade = 50
-export var velocidade = 200
-export var velocidade_max = 100
-export var jump_force = -400
-var movimento = Vector2() 
-var double_jump = 0
-var climbing = false
-var stamina = 10
-export var stamina_max = 10
-
-func _physics_process(_delta):
-	climb()
-	movimento.y += gravidade
-	move_state()
-	stamina_recovery()
-	check_floor()
-	dash()
+var gravity = 100
+var maxFallSpeed = 300
+var walkSpeed = 200
+var dashSpeed = 600
+var dashCooldown = 2 
+var canDash = true
+var isDashing = false
+var canJump = true
+var isJumping = false
+var jumpForce = -350
+var jumpFallForce = 70
+var canClimbing = false
+var isClimbing = false
+var moveVector = Vector2()
+var justTimeInJump = true
+# Inicialização de variáveis
+func _ready():
+	pass
 	
-
-func move_state():
-	if Input.is_action_pressed("ui_right"): 
-		movimento.x = velocidade 
-		$Sprite.play("walk")
-		$Sprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"): 
-		movimento.x = -velocidade
-		$Sprite.play("walk")
-		$Sprite.flip_h = true 
+func _physics_process(delta):
+	gravityProcess()
+	moveProcess()
+	on_ground()
+	moveVector = move_and_slide(moveVector, up)
+	
+	
+func gravityProcess():
+	gravity = 10
+	moveVector.y += gravity 
+	if moveVector.y > maxFallSpeed:
+		moveVector.y = maxFallSpeed
+	
+func moveProcess():
+	if Input.is_action_pressed("ui_right"):
+		moveVector.x = walkSpeed 
+		$playerSprite.play('walk')
+		$playerSprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		moveVector.x = -walkSpeed 
+		$playerSprite.play('walk')
+		$playerSprite.flip_h = true
 	else:
-		movimento.x = 0
-		$Sprite.play("idle")
-	if climbing == true:
-		stamina -= 0.1 
-		if Input.is_action_pressed("ui_up"):
-			movimento.y = -velocidade/1.5 
-		elif Input.is_action_pressed("ui_down"):
-			movimento.y = velocidade/1.5 
-		else: movimento.y = 0
-	if is_on_floor():
-		double_jump = 0
-	if Input.is_action_just_pressed("ui_up") and double_jump <2:
-			movimento.y = jump_force
-			$Sprite.play('jump')
-			double_jump += 1
-	movimento = move_and_slide(movimento, up)
-func climb():	
-	if ($R1.is_colliding() or $R2.is_colliding()) and Input.is_action_pressed("climb") and stamina > 0:
-		gravidade = 0
-		climbing = true
-	else:
-		gravidade = 20
-		climbing = false
-func stamina_recovery():
-	if is_on_floor() and stamina < stamina_max:
-		stamina += 0.1
-func check_floor():
-	if is_on_floor():
-		$Sprite.play('idle')
-
-func dash():
-	if Input.is_action_just_pressed('dash'):
-		velocidade = 600
+		moveVector.x = lerp(moveVector.x,0, 0.35)
+		$playerSprite.play('idle')
+	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or justTimeInJump):
 		$Timer.start()
+		isJumping = true
+		justTimeInJump = false
+		moveVector.y = jumpForce
+	if Input.is_action_just_released('ui_up'):
+		moveVector.y = jumpFallForce 
+func on_ground():
+	if is_on_floor():
+		canJump = true
+		isJumping = false
+		justTimeInJump = true
+	else: 
+		$Timer2.start()
+	#imp gravidade
+	#imp movimentação e flip de sprite
+	#imp animações
+	#imp dash
+	#imp climb
+	#imp stamina
+	#imp stamina ui
+
+
 func _on_Timer_timeout():
-	velocidade = 200
+	canJump = false
+
+
+func _on_Timer2_timeout():
+	canJump = false
