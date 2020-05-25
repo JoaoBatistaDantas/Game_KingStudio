f2400031a16559c0ffcb651dae2a779a3ac6b72c
AllenRodger <44985411+AllenRodger@users.noreply.github.com>
2020-05-25 12:23:14 -0300

    novas mecânicas
    introdução ao dash e just time in jump

--- Game_version2/scripts/Player.gd
+++ Game_version2/scripts/Player.gd
@@ -7,25 +7,27 @@ var maxFallSpeed = 300
 var walkSpeed = 200
 var dashSpeed = 600
 var dashCooldown = 2 
-var canDash = true
+var canDash = false
 var isDashing = false
 var canJump = true
 var isJumping = false
-var jumpForce = -350
+var jumpForce = -400
 var jumpFallForce = 70
 var canClimbing = false
 var isClimbing = false
 var moveVector = Vector2()
 var justTimeInJump = true
+var dashCount = 0
 # Inicialização de variáveis
 func _ready():
 	pass
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	gravityProcess()
 	moveProcess()
 	on_ground()
 	moveVector = move_and_slide(moveVector, up)
+	
 	
 	
 func gravityProcess():
@@ -35,43 +37,55 @@ func gravityProcess():
 		moveVector.y = maxFallSpeed
 	
 func moveProcess():
-	if Input.is_action_pressed("ui_right"):
-		moveVector.x = walkSpeed 
-		$playerSprite.play('walk')
-		$playerSprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-		moveVector.x = -walkSpeed 
-		$playerSprite.play('walk')
-		$playerSprite.flip_h = true
-	else:
-		moveVector.x = lerp(moveVector.x,0, 0.35)
-		$playerSprite.play('idle')
-	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or justTimeInJump):
-		$Timer.start()
-		isJumping = true
-		justTimeInJump = false
-		moveVector.y = jumpForce
-	if Input.is_action_just_released('ui_up'):
-		moveVector.y = jumpFallForce 
+		jump()
+		move()
+		dash()
+	
+	
+	 
 func on_ground():
 	if is_on_floor():
 		canJump = true
 		isJumping = false
 		justTimeInJump = true
-	else: 
-		$Timer2.start()
-	#imp gravidade
-	#imp movimentação e flip de sprite
-	#imp animações
-	#imp dash
-	#imp climb
-	#imp stamina
-	#imp stamina ui
 
+func dash():
+	if canDash and Input.is_action_just_pressed("ui_right"):
+		moveVector.x = 4 * walkSpeed 
+		canDash = false
+	elif canDash and Input.is_action_just_pressed("ui_left"):
+		moveVector.x = 4 * -walkSpeed 
+		canDash = false
+		
+func move():
+	if Input.is_action_pressed("ui_right"):
+		
+		moveVector.x = walkSpeed 
+		canDash = true
+		$CantDashTimer.start()
+		$playerSprite.play('walk')
+		$playerSprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		moveVector.x = -walkSpeed 
+		canDash = true
+		$CantDashTimer.start()
+		$playerSprite.play('walk')
+		$playerSprite.flip_h = true
+	else:
+		moveVector.x = lerp(moveVector.x,0, 0.35)
+		$playerSprite.play('idle')
 
-func _on_Timer_timeout():
-	canJump = false
-
-
-func _on_Timer2_timeout():
-	canJump = false
+func jump():
+	if Input.is_action_just_pressed("ui_up") and ((is_on_floor() or justTimeInJump)):
+		isJumping = true
+		canJump = false
+		justTimeInJump = false
+		moveVector.y = jumpForce
+		$playerSprite.play('jump')
+		
+		
+	if Input.is_action_just_released('ui_up'):
+		moveVector.y = jumpFallForce
+		
+func _on_CantDashTimer_timeout(): # sinal emitido pelo CantDashTimer
+	canDash = false
